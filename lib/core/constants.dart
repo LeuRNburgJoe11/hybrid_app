f52bdff6fc494bafec1139049abafea3973962a1
LeuRNburgJoe11 <j03lrejoices@gmail.com>
2026-06-03 17:21:40 +0800

    constans.dart updated

--- lib/core/constants.dart
+++
@@ -1,8 +0,0 @@
-class AppConstants {
-  AppConstants._();
- 
-  // Your Render.com signaling server URL.
-  // Do NOT append /socket.io — socket_io_client adds that automatically.
-  static const String signalingUrl = "https://hybrid-app-06xq.onrender.com";
-}
- 
